22ebf611901727a9a56ce7da664a6d4145cafecd
eskozin <eskozin@mail.ru>
2020-07-13 10:14:59 +0500

    Add words count feature

--- viseverseproject/views.py
+++ viseverseproject/views.py
@@ -6,4 +6,7 @@ def home(request):
 def reverse(request):
 	user_text = request.GET['usertext']
 	reversed_text = user_text[::-1]
-	return render(request, 'reverse.html', {'usertext':user_text, 'reversedtext': reversed_text})
+	words_count = len(user_text.split())
+	
+	
+	return render(request, 'reverse.html', {'usertext':user_text, 'reversedtext': reversed_text, 'wordscount': words_count})
